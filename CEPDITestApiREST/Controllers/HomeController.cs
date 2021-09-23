883a3e8303ef454fafc205648e10f6ee14367351
Francisco Gomez <francisco.gomez@cepdi.mx>
2021-09-23 15:26:08 -0500

    Se agrega manejo de excepciones

--- CEPDITestApiREST/Controllers/HomeController.cs
+++ CEPDITestApiREST/Controllers/HomeController.cs
@@ -20,65 +20,80 @@ namespace CEPDITestApiREST.Controllers
         }
 
         [HttpPost]
-        public ActionResult TimbrarDocumento(string Usuario, string Password, string Layout)
+        public ActionResult TimbrarDocumento(string Usuario, string Password, string Layout, string UrlServer)
         {
-            string xmlDesencriptado = string.Empty;
-            dynamic clase = new ExpandoObject();
-            clase.Usuario = Encriptar.AESEncrypt(Usuario);
-            clase.Password = Encriptar.AESEncrypt(Password);
-            clase.Lineas = Encriptar.AESEncrypt(Layout);
-
-            var body = JsonConvert.SerializeObject(clase);
-
-            var client = new RestClient("https://localhost:44373/api/Timbrado/TimbrarComprobanteKAVAK");
-            client.Timeout = -1;
-            var request = new RestRequest(Method.POST);
-            request.AddHeader("Content-Type", "application/json");
-            request.AddParameter("application/json", body, ParameterType.RequestBody);
-            IRestResponse response = client.Execute(request);
-            
-            if (response.StatusCode == HttpStatusCode.OK)
+            try
             {
-                var Json = JObject.Parse(response.Content);
-                if (Json.GetValue("exitoso").ToString() == "true")
-                {
-                    xmlDesencriptado = Json.GetValue("xmlTimbrado").ToString();
-                    xmlDesencriptado = Encriptar.AESDecrypt(xmlDesencriptado);
-                }
-                    
-            }
+                string xmlDesencriptado = string.Empty;
+                dynamic clase = new ExpandoObject();
+                clase.Usuario = Encriptar.AESEncrypt(Usuario);
+                clase.Password = Encriptar.AESEncrypt(Password);
+                clase.Lineas = Encriptar.AESEncrypt(Layout);
 
-            return Json(new { HttpStatusCode = response.StatusCode, Content = response.Content, xmlDesencriptado = xmlDesencriptado }, JsonRequestBehavior.AllowGet);
+                var body = JsonConvert.SerializeObject(clase);
+
+                var client = new RestClient(UrlServer);
+                client.Timeout = -1;
+                var request = new RestRequest(Method.POST);
+                request.AddHeader("Content-Type", "application/json");
+                request.AddParameter("application/json", body, ParameterType.RequestBody);
+                IRestResponse response = client.Execute(request);
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    var Json = JObject.Parse(response.Content);
+                    if (Json.GetValue("exitoso").ToString() == "true")
+                    {
+                        xmlDesencriptado = Json.GetValue("xmlTimbrado").ToString();
+                        xmlDesencriptado = Encriptar.AESDecrypt(xmlDesencriptado);
+                    }
+
+                }
+
+                return Json(new { HttpStatusCode = response.StatusCode, Content = response.Content, xmlDesencriptado = xmlDesencriptado }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex) 
+            {
+                return Json(new { HttpStatusCode = 500, Content = ex.Message, xmlDesencriptado = "" }, JsonRequestBehavior.AllowGet);
+            }
         }
 
-        public ActionResult ObtenerPDF(string UUID, string Usuario, string Password)
+        public ActionResult ObtenerPDF(string UUID, string Usuario, string Password, string UrlServer)
         {
-            byte[] PDFDesencriptado = null;
-            dynamic clase = new ExpandoObject();
-            clase.Usuario = Encriptar.AESEncrypt(Usuario);
-            clase.Password = Encriptar.AESEncrypt(Password);
-            clase.UUID = Encriptar.AESEncrypt(UUID);
-
-            var body = JsonConvert.SerializeObject(clase);
-
-            var client = new RestClient("https://localhost:44373/api/Timbrado/ObtenerPDFComprobanteKAVAK");
-            client.Timeout = -1;
-            var request = new RestRequest(Method.POST);
-            request.AddHeader("Content-Type", "application/json");
-            request.AddParameter("application/json", body, ParameterType.RequestBody);
-            IRestResponse response = client.Execute(request);
-
-            if (response.StatusCode == HttpStatusCode.OK)
+            try
             {
-                var Json = JObject.Parse(response.Content);
-                if (Json.GetValue("exitoso").ToString() == "true")
+                byte[] PDFDesencriptado = null;
+                dynamic clase = new ExpandoObject();
+                clase.Usuario = Encriptar.AESEncrypt(Usuario);
+                clase.Password = Encriptar.AESEncrypt(Password);
+                clase.UUID = Encriptar.AESEncrypt(UUID);
+
+                var body = JsonConvert.SerializeObject(clase);
+
+                var client = new RestClient(UrlServer);
+                client.Timeout = -1;
+                var request = new RestRequest(Method.POST);
+                request.AddHeader("Content-Type", "application/json");
+                request.AddParameter("application/json", body, ParameterType.RequestBody);
+                IRestResponse response = client.Execute(request);
+
+                if (response.StatusCode == HttpStatusCode.OK)
                 {
-                    PDFDesencriptado = Encriptar.AESDecryptByte(Json.GetValue("encriptadoPDF").ToString());
+                    var Json = JObject.Parse(response.Content);
+                    if (Json.GetValue("exitoso").ToString() == "true")
+                    {
+                        PDFDesencriptado = Encriptar.AESDecryptByte(Json.GetValue("encriptadoPDF").ToString());
+                    }
+
                 }
 
-            }
+                return Json(new { HttpStatusCode = response.StatusCode, Content = response.Content, PDFDesencriptado = PDFDesencriptado }, JsonRequestBehavior.AllowGet);
 
-            return Json(new { HttpStatusCode = response.StatusCode, Content = response.Content, PDFDesencriptado = PDFDesencriptado }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { HttpStatusCode = 500, Content = ex.Message, xmlDesencriptado = "" }, JsonRequestBehavior.AllowGet);
+            }
         }
     }
 }
